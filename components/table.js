bb9f50692d6f393c121acb714db22d37361e71a4
Imjoma <33196540+Imjoma@users.noreply.github.com>
2023-08-08 19:10:23 +0800

    added HTML Table content

---
+++ components/table.js
@@ -0,0 +1,52 @@
+const tableContainer = document.querySelector(".tableContainer");
+
+const renderTable = `<table>
+<tr>
+<th>This</th>
+<th>is</th>
+  <th>Table</th>
+  <th>Head</th>
+</tr>
+<tr>
+  <td>This</td>
+  <td>is</td>
+  <td>Table</td>
+  <td>Data</td>
+</tr>
+ 
+<tr>
+  <td>table-data1</td>
+  <td>table-data2</td>
+  <td>table-data3</td>
+  <td>table-data4</td>
+</tr>
+</table>
+`;
+
+tableContainer.innerHTML = renderTable;
+
+// STYLE: after render
+const border = "1px solid black";
+
+// ... table
+const table = tableContainer.getElementsByTagName("table")[0];
+
+table.style.width = "60%";
+table.style.border = border;
+table.style.borderCollapse = "collapse";
+
+// ... table head
+const tableHeadRow = table.getElementsByTagName("tr")[0];
+
+tableHeadRow.style.backgroundColor = "#dddddd";
+
+// ... table cells
+const tableData = table.getElementsByTagName("td");
+const tableHeads = table.getElementsByTagName("th");
+
+const cells = [...tableData, ...tableHeads];
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].style.border = border;
+  cells[i].style.padding = ".4rem";
+}
